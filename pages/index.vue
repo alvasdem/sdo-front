c1f222f96ecf1d5cbd1fbf95311bc6d8b6234c02
andysay <info@ddrw.ru>
2020-10-29 18:41:12 +0300

    Форма входа
    Первая версия формы логин

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,31 @@ div
   .layout.p-4
     b-container
       b-row
-        b-col(lg="12" md="9" xl="10").
-          form
+        b-col(lg="12" md="9" xl="10")
+          b-card.o-hidden.border-0.shadow-lg.my-5
+            .card-body.p-0
+              b-row
+                b-col.d-none.d-lg-block.bg-login-image(lg="6")
+                b-col(lg="6")
+                  .p-5(class="text-center")
+                    h1(class="h4 text-gray-900 mb-4") Добро пожаловать!
+                    b-form(@submit='onSubmit' @reset='onReset' v-if='show')
+                      b-form-group#input-group-1(label='Логин:' label-for='input-1' )
+                        b-form-input#input-1(v-model='form.email' type='email' required)
+                      b-form-group#input-group-2(label='Пароль:' label-for='input-2')
+                        b-form-input#input-2(v-model='form.password' type="password" required )
+                      b-form-group#input-group-4
+                        b-form-checkbox-group#checkboxes-4(v-model='form.checked')
+                          b-form-checkbox(value='yes') Запомнить устройство
+                      b-button(type='submit' variant='primary') Войти
+                      hr
+                      | Забыли пароль ?
+                      br
+                      | Создать аккаунт
+
+                    //b-card.mt-3(header='Данные с формы')
+                      pre.m-0 {{ form }}
+
         
         
 </template>
@@ -17,6 +40,35 @@ import Vue from "vue";
 export default Vue.extend({
   head: {
     title: "Главная страница",
+  },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+        checked: [],
+      },
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
   },
 });
 </script>
